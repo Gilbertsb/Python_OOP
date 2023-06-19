2d22dbf54673013226cdd32c99ae510fd9639253
Gilbertsb <gilbert1sib@gmail.com>
2023-06-20 00:15:15 +0200

    First course

---
+++ c2.py
@@ -0,0 +1,8 @@
+class Gilly:
+    def __init__(self,age, name):
+        self.age=age
+        self.name=name
+    
+object2=Gilly(18,"Gilbert")
+print("Thi guy called",object2.name,"is",object2.age,"years old")
+        
